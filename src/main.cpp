bff02ee899fc8b09ffa1044774882747adaf47a4
Rahul <138906783+notcandy001@users.noreply.github.com>
2026-04-25 10:14:17 +0530

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include “server.hpp”
+#include “util.hpp”
+
+int main(int /*argc*/, char * /*argv*/[]) {
+Server server;
+
+```
+if (!server.init()) {
+    LOG_ERROR("Server initialization failed");
+    return 1;
+}
+
+server.run();
+server.destroy();
+
+return 0;
+```
+
+}
